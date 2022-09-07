6066c93e724e9bfaee9c8f33ba717d0ed0b7ac71
AliMDhanani <alidhanani2000@gmail.com>
2022-09-07 15:58:34 +0100

    burger-menu icon implemented

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react';
 
 const Navbar = () => {
+    const [showSidebar, setShowSidebar] = useState(false);
+
   return (
     <div className="navbar container">
         <a href="/" className='logo'>F<span>oo</span>dHeaven</a>
@@ -8,6 +11,11 @@ const Navbar = () => {
             <a href="/">Home</a>
             <a href="/">Recipes</a>
             <a href="/">Settings</a>
+        </div>
+        <div onClick={() => setShowSidebar(!showSidebar)} className={showSidebar ? "sidebar__btn active" :"sidebar__btn"}>
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
         </div>
     </div>
   )
