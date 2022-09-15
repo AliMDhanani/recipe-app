fb5521b865f6adf1d1b17ed9a1072f1c24fd0d1a
AliMDhanani <alidhanani2000@gmail.com>
2022-09-15 14:33:45 +0100

    settings page options initial implementation

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -11,6 +11,39 @@ const Settings = () => {
     "rgb(0, 200, 83)",
     "rgb(156, 39, 176)",
   ];
+
+  const fontSizes = [
+    {
+      title: "Small",
+      value: "12px",
+    },
+    {
+      title: "Medium",
+      value: "16px",
+    },
+    {
+      title: "Large",
+      value: "20px",
+    },
+  ];
+
+  const animationSpeeds = [
+    {
+      title: "Slow",
+      value: 2,
+    },
+    {
+      title: "Medium",
+      value: 1,
+    },
+    {
+      title: "Fast",
+      value: 0.5,
+    },
+  ];
+  const [primaryColor, setPrimaryColor] = useState(0);
+  const [fontSize, setFontSize] = useState(1);
+  const [animationSpeed, setAnimationSpeed] = useState(1);
   return (
     <div>
       <div className="section block">
@@ -32,16 +65,50 @@ const Settings = () => {
           </div>
         </div>
       </div>
+
       <div className="section block">
-        <h2>Preferred Colour</h2>
+        <h2>Colour Theme</h2>
         <div className="options__container">
-          <div className="option light">
-            {theme === "light" && (
-              <div className="tick">
-                <FontAwesomeIcon icon={faCheck} />
-              </div>
-            )}
-          </div>
+          {primaryColors.map((color, index) => (
+            <div className="option light" style={{ backgroundColor: color }}>
+              {primaryColor === index && (
+                <div className="tick">
+                  <FontAwesomeIcon icon={faCheck} />
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="section block">
+        <h2>Font Size</h2>
+        <div className="options__container">
+          {fontSizes.map((size, index) => (
+            <button className="btn">
+              {size.title}
+              {fontSize === index && (
+                <span>
+                  <FontAwesomeIcon icon={faCheck} />
+                </span>
+              )}
+            </button>
+          ))}
+        </div>
+      </div>
+      <div className="section block">
+        <h2>Animation Speed</h2>
+        <div className="options__container">
+          {animationSpeeds.map((speed, index) => (
+            <button className="btn">
+              {speed.title}
+              {animationSpeed === index && (
+                <span>
+                  <FontAwesomeIcon icon={faCheck} />
+                </span>
+              )}
+            </button>
+          ))}
         </div>
       </div>
     </div>
